0eedcfd55db62b4387ccd66789f682054e8c3465
zhanghaojian <1427774041@qq.com>
2024-09-06 21:25:14 +0800

    init project

---
+++ coal-manage-quartz/src/main/java/com/zr/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package com.zr.quartz.util;
+
+import org.quartz.JobExecutionContext;
+import com.zr.quartz.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author ruoyi
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
